51dc8b536e83d79c3a73754708c603ae8d8e94b8
amaan8429 <mohdamaan8429@gmail.com>
2024-04-18 10:36:31 +0530

    posthog added

--- next.config.js
+++ next.config.js
@@ -19,6 +19,18 @@ const config = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
 };
 
 export default config;
