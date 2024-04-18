d6c09bd1b717b079be34a6fe21dabc5b4ff1a4b6
amaan8429 <mohdamaan8429@gmail.com>
2024-04-19 03:34:15 +0530

    testing aias

--- src/env.js
+++ src/env.js
@@ -13,11 +13,6 @@ export const env = createEnv({
       .default("development"),
   },
 
-  /**
-   * Specify your client-side environment variables schema here. This way you can ensure the app
-   * isn't built with invalid env vars. To expose them to the client, prefix them with
-   * `NEXT_PUBLIC_`.
-   */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
